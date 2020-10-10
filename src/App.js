fc01e587e2e88badc53865f669ce922185968de9
Gabriel Gomes <gabrielsogom@gmail.com>
2020-10-10 20:18:31 -0300

    Create home page and style reset

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {ThemeProvider } from 'styled-components'
+
+//Resets navigators' default style parameters
+
+import { Reset} from 'styled-reset'
+
+import theme from './theme';
+import Home from './pages/Home/index'
+
+function App() {
+  return (
+    <ThemeProvider theme={theme}>
+        <Reset/>
+        <Home/>
+        
+    </ThemeProvider>
+  );
+}
+
+export default App;
